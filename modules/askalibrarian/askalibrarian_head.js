8e6958e8cfbf440e9920e0dc6f90826e606c646a
Dan Scott <dscott@laurentian.ca>
2018-04-09 16:33:23 -0400

    Add some CSS for the default chat box
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- modules/askalibrarian/askalibrarian_head.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-  (function() {
-    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?643";
-    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-  })();
-</script>
