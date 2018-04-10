ac2cc244d184961b925b8d071669dde22ffcc806
Dan Scott <dscott@laurentian.ca>
2018-04-10 11:53:47 -0400

    French and English Ask the Library in one module
    * Use JavaScript language detection
    * Remove French "Ask the Library" module
    
    We're all in one now, thanks to the JavaScript-based language detection.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- modules/askalibrarian/askalibrarian.js
+++ modules/askalibrarian/askalibrarian.js
@@ -1,5 +1,9 @@
 (function() {
+    var service = '643';
+    if (document.documentElement['lang'] == 'fr') {
+        service = '531';
+    }
     var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?643";
+    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?" + service;
     var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
   })();
